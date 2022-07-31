53cd2d14d309a78773d3ae43c421f8b5e48a94e5
andrius <andrius.solopovas@gmail.com>
2022-07-31 17:59:45 +0100

    save

--- lyntouch-blocks.php
+++ lyntouch-blocks.php
@@ -5,7 +5,7 @@
  * Description:       A Gutenberg block to show your pride! This block enables you to type text and style it with the color font Gilbert from Type with Pride.
  * Version:           0.1.0
  * Requires at least: 5.9
- * Requires PHP:      8.0
+ * Requires PHP:      7.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
